ddcbfa6316c883f5167c521312310f4c4c6f9db7
César Mora Cid <cesar.mcid@gmail.com>
2019-12-10 22:50:59 -0300

    Agregando capability por defecto y descripción al modulo

---
+++ db/access.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$capabilities = array(
+    'mod/ads:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ),
+);
--- lang/en/ads.php
+++ lang/en/ads.php
@@ -1,5 +1,5 @@
 <?php 
 $string['pluginname'] = 'Firma Digital Avanzada';
 $string['modulename'] = 'Certificado Con Firma Digital';
+$string['modulename_help'] = 'Firma Digital Avanzada permite entregar un certificado firmado digitalmente, el certificado se genera automaticamente al ingresar al modulo. Si no desea entregar el certificado en cualquier momento, debe asegurarse de agregas restricciones de visualización al modulo.';
 $string['modulenameplural'] = 'Certificados Con Firma Digital';
-
