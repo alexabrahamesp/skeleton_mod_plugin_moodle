5cf73c93775633e52a598254c14657bb4c3373b0
César Mora Cid <cesar.mcid@gmail.com>
2019-12-10 23:06:55 -0300

    Parametros de configuración para el recurso

--- lang/en/ads.php
+++ lang/en/ads.php
@@ -3,3 +3,6 @@ $string['pluginname'] = 'Firma Digital Avanzada';
 $string['modulename'] = 'Certificado Con Firma Digital';
 $string['modulename_help'] = 'Firma Digital Avanzada permite entregar un certificado firmado digitalmente, el certificado se genera automaticamente al ingresar al modulo. Si no desea entregar el certificado en cualquier momento, debe asegurarse de agregas restricciones de visualización al modulo.';
 $string['modulenameplural'] = 'Certificados Con Firma Digital';
+$string['certificate_title'] = 'Titulo del certificado';
+$string['certificate_motive'] = 'Motivo del certificado';
+$string['university_name'] = 'Nombre de la universidad';
--- mod_form.php
+++ mod_form.php
@@ -1,31 +1,34 @@
-<?php 
+<?php
+defined('MOODLE_INTERNAL') || die();
 
-// if (!defined('MOODLE_INTERNAL')) {
-//     die('Direct access to this script is forbidden.'); 
-// }
+require_once($CFG->dirroot . '/course/moodleform_mod.php');
+
+class mod_ads_mod_form extends moodleform_mod {
+
+    public function definition() {
+        global $CFG, $DB, $OUTPUT;
+        $strings = [
+            'certificate_title' => get_string('certificate_title', 'ads'),
+            'certificate_motive' => get_string('certificate_motive', 'ads'),
+            'university_name' => get_string('university_name', 'ads'),
+        ];
+
+        $mform =& $this->_form;
+        
+        $mform->addElement('text', 'certificate_title', $strings['certificate_title'], array('size'=>'64'));
+        $mform->setType('certificate_title', PARAM_TEXT);
+        $mform->addRule('certificate_title', null, 'required', null);
+
+        $mform->addElement('text', 'certificate_motive', $strings['certificate_motive'], array('size'=>'64'));
+        $mform->setType('certificate_motive', PARAM_TEXT);
+        $mform->addRule('certificate_motive', null, 'required', null);
+
+        $mform->addElement('text', 'university_name', $strings['university_name'], array('size'=>'64'));
+        $mform->setType('university_name', PARAM_TEXT);
+        $mform->addRule('university_name', null, 'required', null);
  
-// require_once($CFG->dirroot.'/course/moodleform_mod.php');
-// require_once($CFG->dirroot.'/mod/certificate/lib.php');
+        $this->standard_coursemodule_elements();
  
-// class mod_certificate_mod_form extends moodleform_mod {
- 
-//     function definition() {
-//         global $CFG, $DB, $OUTPUT;
- 
-//         $mform =& $this->_form;
- 
-//         $mform->addElement('text', 'name', get_string('certificatename', 'certificate'), array('size'=>'64'));
-//         $mform->setType('name', PARAM_TEXT);
-//         $mform->addRule('name', null, 'required', null, 'client');
- 
-//         $ynoptions = array(0 => get_string('no'),
-//                            1 => get_string('yes'));
-//         $mform->addElement('select', 'usecode', get_string('usecode', 'certificate'), $ynoptions);
-//         $mform->setDefault('usecode', 0);
-//         $mform->addHelpButton('usecode', 'usecode', 'certificate');
- 
-//         $this->standard_coursemodule_elements();
- 
-//         $this->add_action_buttons();
-//     }
-// }
+        $this->add_action_buttons();
+    }
+}
